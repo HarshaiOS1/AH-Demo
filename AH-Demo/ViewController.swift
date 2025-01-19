7e4e9b49e6eada6fdc133aadd3a6829127358bff
HarshaiOS1 <kharsha21@yahoo.in>
2025-01-19 21:46:34 +0100

    Initialised project, removed storyboard dependency

--- AH-Demo/ViewController.swift
+++ AH-Demo/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy private var sampleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello AH!"
+        label.textColor = .red
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
+        self.view.addSubview(self.sampleLabel)
+        self.setUpConstraints()
     }
-
-
+    
+    func setUpConstraints() {
+        let sampleLabelConstraints = [
+            self.sampleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.sampleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ]
+        NSLayoutConstraint.activate(sampleLabelConstraints)
+    }
+    
 }
-
